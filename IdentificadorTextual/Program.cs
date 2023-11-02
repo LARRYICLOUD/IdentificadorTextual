5315f48802dc91ee8316c75ec8ce590b4273aa74
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2023-11-01 21:11:51 -0500

    nueva

--- IdentificadorTextual/Program.cs
+++ IdentificadorTextual/Program.cs
@@ -13,11 +13,30 @@ namespace IdentificadorTextual
             Console.WriteLine("YO SOY UN PROGRAMADOR Y EXPERTO EN REDES EXCELENTE Y RECONOCIDO");
             //Declarando un objeto DateTime
             DateTime fechaHoraPredeterminados = new DateTime();
-            DateTime fechaHora = new DateTime(2023, 10, 31, 20, 55, 32);
+            DateTime fechaHora = new DateTime(2023, 11, 1, 20, 55, 32);
             DateTime fecha = new DateTime(2023, 10, 31);
             Console.WriteLine(fechaHoraPredeterminados);
-            Console.WriteLine(fechaHora);
+            Console.WriteLine($"El valor del objeto DateTime es: { fechaHora}");
             Console.WriteLine(fecha);
+
+
+            //Convirtiendo el objeto DateTime en una cadena
+            //Especificadores que requieren modificacion ("d", "f", "F", "g", "h", "H","k", "m", "M", "s", "t", "y", "z", ":", o "/" )
+            // colocando espacio antes o despues y el signo de porcentaje nos muestra el dia del 1 al 31 segun tengamos en la fecha designada
+
+            //"ddd" nos muestra un dia abreviado
+
+            //string cadenaFechaHora = fechaHora.ToString("%d");
+            //string cadenaFechaHora = fechaHora.ToString("ddd");
+            //string cadenaFechaHora = fechaHora.ToString("d ");
+            //string cadenaFechaHora = fechaHora.ToString(" d");
+            string cadenaFechaHora = fechaHora.ToString("dddd dd MMMM yyyy h:m:s tt");
+
+            //Convirtiendo la representacion encadena del objeto DateTime segun la referencia cultural actual (es-MX)
+            Console.WriteLine($"La presentacion en cadena es: {cadenaFechaHora}");
+
+
+
 
         }
     }
